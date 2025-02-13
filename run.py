b60397ea5996b9cb6e84cefd3b93b4d13b659029
Ho3einMokhtari <hosseinmokhtari5555@gmail.com>
2025-02-13 18:00:07 +0330

    v1.0.0

--- run.py
+++ run.py
@@ -4,66 +4,58 @@ import time
 import os
 import platform
 
-
 def install_requirements():
-    print("Installing dependencies from requirements.txt...")
+    print("درحال نصب نیازمندی ها...")
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
 def open_chrome():
-    print("Opening Google Chrome with remote-debugging-port=9222...")
-    
-    # Detect platform and set the appropriate path to Chrome executable
+    print("در حال باز کردن مرورگر کروم...")
     system_platform = platform.system()
     
     if system_platform == "Windows":
-        chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"  # Update path if needed
-    elif system_platform == "Darwin":  # macOS
+        chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"
+    elif system_platform == "Darwin":
         chrome_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
     elif system_platform == "Linux":
-        chrome_path = "/usr/bin/google-chrome"  # Or "/usr/bin/chromium-browser", depending on your system setup
+        chrome_path = "/usr/bin/google-chrome"
     else:
         raise Exception(f"Unsupported platform: {system_platform}")
     
-    # Run Chrome with remote-debugging-port
     subprocess.Popen([chrome_path, "--remote-debugging-port=9222"])
 
 def run_celery_worker():
-    print("Starting Celery worker...")
-    # Change to the 'workers' folder before running the command
+    print("در حال اجرای سلری...")
     os.chdir("workers")
     subprocess.Popen(["celery", "-A", "methods", "worker", "--loglevel=info", "--pool=solo"])
 
 def run_fastapi():
-    print("Starting FastAPI server...")
-    # Make sure we are still in the 'workers' folder for the FastAPI command
+    print("در حال اجرای وب سرویس...")
     subprocess.Popen(["uvicorn", "api:app"])
 
 def run_streamlit():
-    print("Starting Streamlit panel...")
-    # After finishing the workers commands, go back to the main folder to run Streamlit
-    os.chdir("..")  # Go back to the main directory (assuming Streamlit is in the root directory)
+    print("درحال اجرای پنل مدیریت...")
+    os.chdir("..")
     subprocess.Popen(["streamlit", "run", "main_page.py"])
 
 def main():
-    # Install dependencies
+    # نصب نیازمندی ها
     install_requirements()
 
-    # Open Google Chrome
+    # باز کردن کروم
     open_chrome()
+    # مقداری صبر بعد از باز شدن کروم
+    time.sleep(1)
 
-    # Wait a bit before starting the services to ensure Chrome is ready
-    time.sleep(5)
-
-    # Run Celery worker in the 'workers' folder
+    # اجرای سلری
     run_celery_worker()
 
-    # Run FastAPI server in the 'workers' folder
+    # اجرای fastApi
     run_fastapi()
 
-    # Run Streamlit panel from the main folder
+    # اجرای پنل مدیریت
     run_streamlit()
     
-    print("All services are up and running!")
+    print("تمام سرویس ها با موفقیت اجرا شد!")
 
 if __name__ == "__main__":
     main()
